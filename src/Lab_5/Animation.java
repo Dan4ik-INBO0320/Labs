be80bb21ac0dc4eb67c6dc7683282b35fe7d436d
Dan4ik <lader.ggwp@mail.ru>
2021-10-18 23:14:59 +0300

    Complete Lab_5

--- src/Lab_5/Animation.java
+++ src/Lab_5/Animation.java
@@ -2,73 +2,69 @@ package Lab_5;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 
 public class Animation extends JFrame {
 
-    private int k = 0;
+    JPanel grid = new JPanel();
 
-    JLabel znach = new JLabel("Значение k: " + k);
-    JButton button = new JButton("Прибавить 1 к k");
+    ImageIcon icon1 = new ImageIcon(getClass().getResource("1.jpg"));
+    ImageIcon icon2 = new ImageIcon(getClass().getResource("2.jpg"));
+    ImageIcon icon3 = new ImageIcon(getClass().getResource("3.jpg"));
+    ImageIcon icon4 = new ImageIcon(getClass().getResource("4.jpg"));
+    ImageIcon icon5 = new ImageIcon(getClass().getResource("5.jpg"));
+    ImageIcon icon6 = new ImageIcon(getClass().getResource("6.jpg"));
+    JLabel label1 = new JLabel(icon1);
+    JLabel label2 = new JLabel(icon2);
+    JLabel label3 = new JLabel(icon3);
+    JLabel label4 = new JLabel(icon4);
+    JLabel label5 = new JLabel(icon5);
+    JLabel label6 = new JLabel(icon6);
 
-    Animation()
-    {
 
-        super("Example");
-        setLayout(new BorderLayout());
-        setSize(500,450);
+    Animation() throws InterruptedException {
+
+        super("Animation");
+
+        setLayout(new GridLayout());
+        setSize(610,470);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                kPlus();
-                znach.setText("Значение k: " + k);
-            }
 
-            @Override
-            public void mousePressed(MouseEvent e) {
+        GridLayout layout = new GridLayout(2, 0, 0, 0);
+        grid.setLayout(layout);
 
-            }
+        grid.add(label1);
+        getContentPane().add(grid);
+        revalidate();
+        Thread.sleep(2000);
+        grid.add(label2);
+        getContentPane().add(grid);
+        revalidate();
+        Thread.sleep(2000);
+        grid.add(label3);
+        getContentPane().add(grid);
+        revalidate();
+        Thread.sleep(2000);
+        grid.add(label4);
+        getContentPane().add(grid);
+        revalidate();
+        Thread.sleep(2000);
+        grid.add(label5);
+        getContentPane().add(grid);
+        revalidate();
+        Thread.sleep(2000);
+        grid.add(label6);
+        getContentPane().add(grid);
+        revalidate();
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-
-        add(znach, BorderLayout.WEST);
-        znach.setText("Значение k: " + k);
-        add(button, BorderLayout.EAST);
+        getContentPane().add(grid);
 
     }
 
-    public void kPlus() {
-        k++;
-    }
 
-    public void setK(int k) {
-        this.k = k;
-    }
-
-
-
-
-
-    public static void main(String[]args)
-    {
+    public static void main(String[]args) throws InterruptedException {
         new Animation();
     }
 }
